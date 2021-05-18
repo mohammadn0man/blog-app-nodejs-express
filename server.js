e82066963fb0de5103694518857f74bfe5c2f148
Mohammad Noman <mohammad.noman@live.in>
2021-05-18 16:40:49 +0530

    First commit with project init with db connection and form view

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const Article = require("./models/article")
+const articleRouter = require("./routes/articles")
+const methodOverriden = require('method-override')
+const app = express()
+
+const uri = 'mongodb+srv://dbUser:passw0rd@cluster0.iklsa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverriden('_method'))
+
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({ createdAt: 'desc' })
+    res.render('index', { articles: articles })
+})
+
+app.use('', articleRouter)
+
+app.listen(5001)
